653819ddf0dc3fca2ce569aca2db0495431639dc
mpi-eherald <eherald@magnumpiering.com>
2026-09-14 14:13:40 -0400

    update decommissioned

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import httpx
+from benedict import benedict
 from dotenv import load_dotenv
 from flask import Flask, render_template
 from flask_apscheduler import APScheduler
@@ -18,10 +19,10 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 scheduler = APScheduler()
-scheduler.init_app(app)
+scheduler.init_app(app) 
 
 
-@scheduler.task('cron', id='run_sync_once_per_minute', minute=0)
+@scheduler.task('cron', id='run_sync_once_per_minute', minute="*")
 def sync_asset_status():
   global assetStatus
   print("Syncing MaintainX asset status")
@@ -36,17 +37,49 @@ def sync_asset_status():
   assets = r.json()["assets"]
 
   results = []
-  
+    
   for asset in assets:
-    if asset["status"]["status"] == "OFFLINE":
-      results.append({
+    a = benedict(asset)
+    try:
+      if 'status.customStatus' in a:
+        results.append({
           "id": asset["id"],
           "name": asset["name"],
-          "status": asset["status"]["status"],
+          "status": asset["status"]["customStatus"]["label"].upper(),
           "downtimeType": asset["status"]["downtimeType"],
           "description": asset["status"]["description"],
           "startedAt": dt.fromisoformat(asset["status"]["startedAt"]).date()
         })
+        continue
+    except:
+      pass
+
+    # if asset["status"]["customStatus"] in asset:
+    #   results.append({
+    #     "id": asset["id"],
+    #     "name": asset["name"],
+    #     "status": asset["status"]["customStatus"]["label"],
+    #     "downtimeType": asset["status"]["downtimeType"],
+    #     "description": asset["status"]["description"],
+    #     "startedAt": dt.fromisoformat(asset["status"]["startedAt"]).date()
+    #   })
+
+    if a["status"]["status"] == "OFFLINE":
+      results.append({
+          "id": a["id"],
+          "name": a["name"],
+          "status": a["status"]["status"],
+          "downtimeType": a["status"]["downtimeType"],
+          "description": a["status"]["description"],
+          "startedAt": dt.fromisoformat(a["status"]["startedAt"]).date()
+        })
+      continue
+
+    results.append({
+      "id": a["id"],
+      "name": a["name"],
+      "status": a["status"]["status"]
+    })
 
   assetStatus = sorted(results, key=lambda x: x["status"])
   print(f"Success: synced {len(assets)} assets")
